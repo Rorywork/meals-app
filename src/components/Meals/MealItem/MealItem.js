6ea3d2f13203b89c04274b9147c1388740d22c44
Rory <rory.lindsay@pulselive.com>
2023-01-18 15:26:38 +0000

    feat: create input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,9 @@
-import classes from './MealItem.module.css';
+import MealItemForm from "./MealItemForm";
+
+import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
-const price = `$${props.price.toFixed(2)}`
+  const price = `$${props.price.toFixed(2)}`;
 
   return (
     <li className={classes.meal}>
@@ -11,7 +13,7 @@ const price = `$${props.price.toFixed(2)}`
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
